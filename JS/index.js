c1bb711655f489fe52450c8a71177e2dc27164ac
Fabien laurent <fabien.laurent59@outlook.com>
2020-11-12 09:55:35 +0100

    Revert "first release home page"
    This reverts commit dc4852f3487cef68271bb582ca1259975a8c01bb.

--- JS/index.js
+++
@@ -1,7 +0,0 @@
-function loaded(callable){document.addEventListener("DOMContentLoaded", callable);}
-function $(selector){return document.querySelector(selector);}
-function $$(selector){return document.querySelectorAll(selector);}
-
-loaded(function(){
-    console.log("DOM chargé");
-});
